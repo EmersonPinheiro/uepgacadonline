689c4a81657ed33170a78b50ec70f79bb4fd9bff
Paulo Alves <juninhpaulo@hotmail.com>
2018-03-23 17:41:10 -0300

    Wrapper functions

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const {app, BrowserWindow} = require('electron')
+const uepgwrapper = require('./uepgwrapper')
+
+uepgwrapper.getHeaders((headers, callback) => {
+    uepgwrapper.getAuth(headers, (headers, callback) => {
+        uepgwrapper.getDocs(headers, (body) => {
+            console.log(body)
+        })
+    })
+})
+
+/*
+
+const { app, BrowserWindow } = require('electron')
 
 app.on('ready', () => {
     let mainWindow = new BrowserWindow({
@@ -6,9 +18,12 @@ app.on('ready', () => {
         height: 400
     })
 
-    mainWindow.loadURL('https://sistemas.uepg.br/academicoonline/login/index')
+    //mainWindow.loadURL('https://sistemas.uepg.br/academicoonline/login/index')
+    mainWindow.loadURL('file://src/index.html')
 })
 
 app.on('window-all-closed', () => {
     app.quit()
 })
+
+*/
---
+++ uepgwrapper.js
@@ -0,0 +1,136 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+module.exports = {
+    getHeaders(callback) {
+        request({
+            method: 'GET',
+            url: 'https://sistemas.uepg.br/academicoonline/login/index',
+        }, (error, resp, body) => {
+            headers = {
+                'cookie': `${resp.headers['set-cookie'][0].split(';')[0]}; __utma=241181661.1990841241.1518980587.1520085361.1520277780.4; __utmz=241181661.1518980587.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)`,
+                'connection': 'keep-alive',
+                'upgrade-insecure-requests': "1"
+            }
+            callback(headers, callback)
+        })
+    },
+
+    getAuth(headers, callback) {
+        request({
+            method: 'POST',
+            url: 'https://sistemas.uepg.br/academicoonline/login/authenticate',
+            form: { 'login': '14147326', 'password': '' },
+            headers
+        }, (error, resp, body) => {
+            callback(headers, callback)
+        })
+    },
+
+    getGrade(headers, callback) {
+        request({
+            method: 'GET',
+            url: 'https://sistemas.uepg.br/academicoonline/avaliacaoDesempenho/index',
+            headers
+        }, (error, resp, body) => {
+            callback(body)
+        })
+    },
+
+    parseGrade(body) {
+        const $ = cheerio.load(body)
+
+        let t = []
+        $('table').find('td').each((index, element) => {
+            let children = $(this).children()
+            let itemNum = children.eq(0);
+
+            var r = {
+                "Num": itemNum.text().trim()
+            };
+
+            t.push(r)
+        })
+
+        return t
+
+        //return $('table td').html()
+    },
+
+    getDocs(headers, callback) {
+        request({
+            method: 'GET',
+            url: 'https://sistemas.uepg.br/academicoonline/documentos/index',
+            headers
+        }, (error, resp, body) => {
+            callback(body)
+        })
+    },
+
+    parseDocs(body) {
+        const $ = cheerio.load(body)
+
+        let t = []
+        $('.fieldcontain').each((index, element) => {
+            t.push(element.data)
+        })
+
+        return t
+    },
+
+    getPerfil(headers, callback) {
+        request({
+            method: 'GET',
+            url: 'https://sistemas.uepg.br/academicoonline/academico_pessoa/edit_validado',
+            headers
+        }, (error, resp, body) => {
+            callback(body)
+        })
+    },
+
+    parsePerfil(body) {
+
+    },
+
+    getCenso(headers, callback) {
+        request({
+            method: 'GET',
+            url: 'https://sistemas.uepg.br/academicoonline/academico_cor_raca_deficiencia/show/6162',
+            headers
+        }, (error, resp, body) => {
+            callback(body)
+        })
+    },
+
+    parseCenso(body) {
+        return body
+    },
+
+    getAtivCompl(headers, callback) {
+        request({
+            method: 'GET',
+            url: 'https://sistemas.uepg.br/academicoonline/atividadeComplementar/index',
+            headers
+        }, (error, resp, body) => {
+            callback(body)
+        })
+    },
+
+    parseAtivCompl(body) {
+
+    },
+
+    getDownloads(headers, callback) {
+        request({
+            method: 'GET',
+            url: 'https://sistemas.uepg.br/academicoonline/downloads/index',
+            headers
+        }, (error, resp, body) => {
+            callback(body)
+        })
+    },
+
+    parseDownloads(body) {
+        return body
+    }
+}
