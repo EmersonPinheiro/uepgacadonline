09cbf5ce58741c7a1d4f2dc597e347fb8828ecd4
Paulo Alves <juninhpaulo@hotmail.com>
2018-03-25 15:45:31 -0300

    Making everything static :(

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ const { app, BrowserWindow } = require('electron')
 app.on('ready', () => {
     let mainWindow = new BrowserWindow({
         width: 800,
-        height: 8   00
+        height: 600
     })
-
-    //mainWindow.loadURL('https://sistemas.uepg.br/academicoonline/login/index')
+    
     mainWindow.loadURL(`file://${__dirname}/src/login.html`)
-    //mainWindow.loadURL('https://dontfile.herokuapp.com')
 })
 
 app.on('window-all-closed', () => {
