e531e183f1a6aa0ea3c28ee540c72cb11fa14090
Paulo Alves <juninhpaulo@hotmail.com>
2018-03-25 07:49:14 -0300

    Sample Table and List.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const { app, BrowserWindow } = require('electron')
 
 app.on('ready', () => {
     let mainWindow = new BrowserWindow({
-        width: 600,
-        height: 400
+        width: 1000,
+        height: 600
     })
 
     //mainWindow.loadURL('https://sistemas.uepg.br/academicoonline/login/index')
